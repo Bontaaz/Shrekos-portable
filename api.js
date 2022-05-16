09b97bce1a78be6c6e6f47f12aaae5469367e375
Jaleel <jaleel.aouadj@ynov.com>
2022-05-16 09:47:49 +0200

    mama

--- api.js
+++ api.js
@@ -1,6 +1,6 @@
-const api = fetch('https://rawcdn.githack.com/akabab/superhero-api/0.2.0/api/all.json').then((response) => response.json()).then(data => console.log(data))
+const api = fetch('https://rawcdn.githack.com/akabab/superhero-api/0.2.0/api/all.json').then((response) => response.json()).then(data => LoadData(data))
 
 
-function LoadData(){
-    
+function LoadData(data){
+    console.log(data)
 }
