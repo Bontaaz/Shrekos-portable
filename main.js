ff9d81ad8b39abde4f27a96edcb8c99568bc41d7
scappe <sean.cappe@ynov.com>
2022-05-18 15:59:59 +0200

    m

--- main.js
+++ main.js
@@ -1,23 +1,42 @@
 import { Grossefonction as api } from "./api.js";
 const tableau = document.getElementById('tbody')
-let data
-let tabdata = [`name`,`appearance.gender`,`appearance.race`,`appearance.weight[0]`]
-
+let data 
+const input = document.getElementById('myInput')
 api().then((res) => {
     data = res
+    
     data.forEach(element => {
-        console.log(element)
+        let tabdata = [`${element.name}`,`${element.appearance.gender}`,`${element.appearance.race}`,`${element.appearance.weight[0]}`,`${element.appearance.height[0]}`,`${element.biography.placeOfBirth}`,`${element.biography.alignment}`,`${element.powerstats[0]}`]
         let row = document.createElement('tr')
-        console.log(tabdata[0])
-        for (let index = 0; index < 4; index++) {
+        for (let index = 0; index < 8; index++) {
             let ele = document.createElement('td')
-            let text3 = document.createTextNode(`${elementabdata[index]}`)
+            let text3 = document.createTextNode(`${tabdata[index]}`)
             ele.appendChild(text3)
             row.appendChild(ele)
         }
         tableau.appendChild(row)
     });
-
-
-
+    
 })
+
+input.oninput = function(){
+        // Declare variables
+        let input, filter, table, tr, td, i, txtValue;
+        input = document.getElementById("myInput");
+        filter = input.value.toUpperCase();
+        table = document.getElementById("myTable");
+        tr = table.getElementsByTagName("tr");
+        console.log('prout')
+        // Loop through all table rows, and hide those who don't match the search query
+        for (i = 0; i < tr.length; i++) {
+          td = tr[i].getElementsByTagName("td")[0];
+            if (td) {
+                txtValue = td.textContent || td.innerText;
+                if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                    tr[i].style.display = "";
+                } else {
+                    tr[i].style.display = "none";
+                }
+            }
+        }
+}
